1a37aa3e242156f0d6da373d5e960c62e79f7691
aureliasanchez <aureliasancheze5@gmail.com>
2024-06-18 12:18:58 -0700

    numeros sitios 18

--- Rancho/whatsapp.py
+++ Rancho/whatsapp.py
@@ -27,10 +27,10 @@ def enviar_mensajes(datos, browser_path, image_path_coords, drop_area_coords):
         # Si hay una imagen, se adjunta la imagen antes de enviar el mensaje de texto
         if pd.notna(imagen):
             # Hacer clic en el botón de adjuntar (clip)
-            pyautogui.click(538, 673)
+            pyautogui.click(535, 676)
             time.sleep(3)
             # Hacer clic en el botón de adjuntar imagen
-            pyautogui.click(558, 322)
+            pyautogui.click(563, 324)
             time.sleep(3)
             
             # Copiar la ruta de la imagen al portapapeles
@@ -43,29 +43,29 @@ def enviar_mensajes(datos, browser_path, image_path_coords, drop_area_coords):
             
             # Presionar ENTER para seleccionar la imagen
             pyautogui.press('enter')
-            time.sleep(5)  # Esperar a que la imagen se cargue en el chat
+            time.sleep(3)  # Esperar a que la imagen se cargue en el chat
             
         # Presionar ENTER para enviar el mensaje de texto (y la imagen si existe)
         pyautogui.press('enter')
         print(f"Mensaje y imagen enviados a {movil}.")
-        time.sleep(10)  # Esperar a que el mensaje y la imagen se envíen
+        time.sleep(20)  # Esperar a que el mensaje y la imagen se envíen
         
         # Cierra la pestaña actual
         pyautogui.hotkey('ctrl', 'w')
         
         # Espera aleatoria antes de pasar al siguiente contacto
-        tiempo_espera = random.uniform(6, 10)
+        tiempo_espera = random.uniform(4, 8)
         print(f"Esperando {tiempo_espera:.2f} segundos antes de pasar al siguiente contacto...")
         time.sleep(tiempo_espera)
 
 # Leer los datos del archivo de contactos, mensajes e imágenes
-datos = pd.read_excel("listaContactos.xlsx")
+datos = pd.read_excel("asignaciones.xlsx")
 
 # Coordenadas del área de la ruta de la imagen (ajustar según sea necesario)
-image_path_coords = (408, 165)  # Ajustar según la posición real de la imagen en tu sistema de archivos
+image_path_coords = (535, 676)  # Ajustar según la posición real de la imagen en tu sistema de archivos
 
 # Coordenadas del área de drop en el chat de WhatsApp Web
-drop_area_coords = (780, 663)  # Ajustar según la posición real del área de drop en el chat de WhatsApp Web
+drop_area_coords = (563, 324)  # Ajustar según la posición real del área de drop en el chat de WhatsApp Web
 
 # Ruta al navegador específico
 browser_path = 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s'
