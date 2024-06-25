3c62519e72d13a9b5604fd63740065e8f7c65455
aureliasanchez <aureliasancheze5@gmail.com>
2024-06-24 18:16:56 -0600

    24

--- Rancho/whatsapp.py
+++ Rancho/whatsapp.py
@@ -27,10 +27,10 @@ def enviar_mensajes(datos, browser_path, image_path_coords, drop_area_coords):
         # Si hay una imagen, se adjunta la imagen antes de enviar el mensaje de texto
         if pd.notna(imagen):
             # Hacer clic en el botón de adjuntar (clip)
-            pyautogui.click(557, 825)
+            pyautogui.click(499, 826)
             time.sleep(3)
             # Hacer clic en el botón de adjuntar imagen
-            pyautogui.click(608, 473)
+            pyautogui.click(588, 479)
             time.sleep(3)
             
             # Copiar la ruta de la imagen al portapapeles
